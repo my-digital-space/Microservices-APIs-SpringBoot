cb4d29dfee21e359c879349f5cb43595bac4c2b5
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-10-25 17:41:12 +0530

    initial commit

---
+++ RetryPattern1/src/main/java/com/ms/RetryPattern1/RetryPattern1Application.java
@@ -0,0 +1,13 @@
+package com.ms.RetryPattern1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryPattern1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryPattern1Application.class, args);
+	}
+
+}
---
+++ RetryPattern1/src/test/java/com/ms/RetryPattern1/RetryPattern1ApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.ms.RetryPattern1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RetryPattern1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
